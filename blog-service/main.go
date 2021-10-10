df4f3730c2a923828f0dbf39010220eaaa423358
kaindy7633 <kaindy7633@gmail.com>
2021-10-10 15:15:31 +0800

    更新自定义错误码部分

--- blog-service/main.go
+++ blog-service/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/kaindy7633/go-programming-tour-book/blog-service/internal/routers"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
-	})
-	_ = r.Run()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	if err := s.ListenAndServe(); err != nil {
+		fmt.Println("happened some err: ", err)
+	}
 }
